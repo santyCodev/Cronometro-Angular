3517c0dd0d394d0a9fca4a17d735b4bda6498603
santy <santi.codev@gmail.com>
2020-11-19 11:21:08 +0100

    Disparo de eventos desde el componente hijo al padre

--- CronometroAngular/src/app/app.module.ts
+++ CronometroAngular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CronometroComponent } from './components/cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule
